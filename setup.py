25b4bb800b55d3a8877d0a7f315e91eb1a36b8b3
Konrad Jałowiecki <dexter2206@gmail.com>
2018-04-19 02:41:17 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""pylttb - downsampling of given series using Largest Triangle Three Buckets method."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='pylttb',
+    license='MIT',
+    description=__doc__,
+    use_scm_version=True,
+    long_description=LONG_DESCRIPTION,
+    platforms=["Linux", "Unix"],
+    setup_requires=['setuptools_scm'],
+    install_requires=['numpy'],
+    tests_require=['marshmallow', 'pyaml', 'ddt'],
+    author='Konrad Jałowiecki <dexter2206@gmail.com>',
+    author_email='dexter2206@gmail.com',
+    packages=find_packages(),
+    keywords='downsampling'
+)
